a901116d262cc0ebd7974be8925c17df6a4a7fbe
Jerry Li <jerry.limagic0302@gmail.com>
2022-12-10 15:45:01 -0500

    bitmap

--- borderDisplay.cc
+++ borderDisplay.cc
@@ -13,8 +13,8 @@ BorderDisplay::~BorderDisplay() {}
 void BorderDisplay::doDisplay() {
     shared_ptr<Border> border = make_shared<Border>();
     shared_ptr<Bitmap> map = border->getBorder();
-    vector<shared_ptr<tuple<int, int, char>>> borderArray = map->getMap();
+    vector<tuple<int, int, char>> borderArray = map->getMap();
     for (int i = 0; i < borderArray.size(); i++) {
-        mvaddch(std::get<0>(*borderArray[i]), std::get<1>(*borderArray[i]), std::get<2>(*borderArray[i]));
+        mvaddch(get<0>(borderArray[i]), get<1>(borderArray[i]), get<2>(borderArray[i]));
     }
 }   
