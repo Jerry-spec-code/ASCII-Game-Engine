18f6ecc78bbf1c4265f21aefbeb8e72cacc50731
Jerry Li <jerry.limagic0302@gmail.com>
2022-12-10 13:32:28 -0500

    border

--- borderDisplay.cc
+++ borderDisplay.cc
@@ -1,5 +1,8 @@
 #include "borderDisplay.h"
+#include "bitmap.h"
+#include "border.h"
 #include <ncurses.h>
+#include <memory>
 
 #include <iostream>
 using namespace std;
@@ -8,6 +11,12 @@ BorderDisplay::BorderDisplay() {}
 BorderDisplay::~BorderDisplay() {}
 
 void BorderDisplay::doDisplay() {
-    // cout << "Made it here " << endl;
+    shared_ptr<Border> border = make_shared<Border>();
+    WINDOW *win = newwin(border->getBorderWidth(), border->getBorderLength(), 0, 0);
     printw("Hello World !!!");	/* Print Hello World		  */
+    shared_ptr<Bitmap> map = border->getBorder();
+    vector<shared_ptr<tuple<int, int, char>>> borderArray = map->getMap();
+    for (int i = 0; i < borderArray.size(); i++) {
+        mvaddch(std::get<0>(*borderArray[i]), std::get<1>(*borderArray[i]), std::get<2>(*borderArray[i]));
+    }
 }   
