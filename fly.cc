833fda615e8dfaf6f251ff891f2de6f1b4433c55
jw25li <jw25li@uwaterloo.ca>
2022-12-12 13:44:42 -0500

    make

--- fly.cc
+++ fly.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 Fly::Fly() {}
-Fly::Fly(char c, int length, int width, int x, int y, int height = 1): 
+Fly::Fly(char c, int length, int width, int x, int y, int height): 
 Rectangle{c, length, width, x, y, height} {}
 
 Fly::~Fly() {}
