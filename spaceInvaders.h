fe8db696acd0a81e5509268da47d98df2b3cdb13
Jerry Li <jerry.limagic0302@gmail.com>
2022-12-13 11:06:23 -0500

    timer

--- spaceInvaders.h
+++ spaceInvaders.h
@@ -18,7 +18,7 @@ class SpaceInvaders : public Game {
     shared_ptr<Border> border;
     int status = 1;
     int aliensKilled = 0;
-    int updateInterval = 100000;
+    int updateInterval = 500000;
 public:
     SpaceInvaders();
     ~SpaceInvaders();
