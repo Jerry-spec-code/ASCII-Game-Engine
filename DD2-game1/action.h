e09d3d86181826407cb31f5d513966855191cb4f
jw25li <jw25li@uwaterloo.ca>
2022-12-13 18:17:43 -0500

    ice cream

--- DD2-game1/action.h
+++
@@ -1,6 +0,0 @@
-#ifndef ACTION_H
-#define ACTION_H
-
-enum Action { INVALID = 0, UP, RIGHT, DOWN, LEFT, NONE, CW, CCW};
-
-#endif
--- DD2-game1/spaceAlien.cc
+++
@@ -1,6 +0,0 @@
-#include "spaceAlien.h"
-
-Alien::Alien(char c, int x, int y, int height): Character{c, x, y, height} {}
-
-Alien::~Alien() {}
-
