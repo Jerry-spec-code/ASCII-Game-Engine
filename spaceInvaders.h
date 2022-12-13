bffb00a58d7e2defc1495b69526559f7cf4f42a6
Jerry Li <jerry.limagic0302@gmail.com>
2022-12-13 13:11:45 -0500

    view

--- spaceInvaders.h
+++ spaceInvaders.h
@@ -19,7 +19,7 @@ class SpaceInvaders : public Game {
     int status = 1;
     int aliensKilled = 0;
     int offset = 2;
-    int updateInterval = 2500000;
+    int updateInterval = 1000000;
 public:
     SpaceInvaders();
     ~SpaceInvaders();
